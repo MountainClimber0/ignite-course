526e999f716530fa7ad0d2b3b018deac92e8eace
Miguel Moreira <87150446+MountainClimber0@users.noreply.github.com>
2022-11-22 16:00:34 -0400

    sidebar-creation

--- iniciando-react/aulas/01-fundamentos-reactjs/src/App.jsx
+++ iniciando-react/aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,16 +1,23 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
     <div>
       <Header />
 
-      <Post
-        author="Diego fernandes"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse iste quia eligendi fugiat minima corrupti! Dignissimos molestias eius error laudantium suscipit tempora necessitatibus unde, mollitia nam, adipisci delectus enim accusantium."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post
+            author="Diego fernandes"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse iste quia eligendi fugiat minima corrupti! Dignissimos molestias eius error laudantium suscipit tempora necessitatibus unde, mollitia nam, adipisci delectus enim accusantium."
+          />
+        </main>
+      </div>
     </div>
   );
 }
